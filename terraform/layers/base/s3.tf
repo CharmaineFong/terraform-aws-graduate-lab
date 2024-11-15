1626c943686494e8cf2a3c6db3a13fa07d34c362
Charmaine Fong <charmainefong@Charmaines-MacBook-Pro.local>
2024-11-15 19:37:05 +0000

    s3.tf added to store ec2 website files

--- terraform/layers/base/s3.tf
+++ terraform/layers/base/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "${var.project_name}-${var.environment}-ec2-web-files"
+  bucket = "${var.project_name}-${var.environment}-ec2-website-files"
 
   versioning = {
     enabled = true
