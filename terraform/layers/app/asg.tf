7cbb1bfb14354ce335c0bed9764e090c332b9364
Charmaine Fong <charmainefong@Charmaines-MacBook-Pro.local>
2024-11-18 19:06:10 +0000

    launch template configuretd to use user data script

--- terraform/layers/app/asg.tf
+++ terraform/layers/app/asg.tf
@@ -4,7 +4,7 @@
 # TF Code added to app layer asg.tf
 # image_id obtained by using data source to retrieve latest AMI ID
 # src folder copied from ~/src/lab1 to app layer
-# Launch Template configured to use user data script ec2WebUserData.tftpl
+
 # ASG Configuration matches lab requirements
 
 # Amazon Linux 2 AMIs and sized as t2.micro
@@ -17,4 +17,20 @@ data "aws_ami" "latest_linux_2_AMI" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+}
+
+# Launch Template configured to use user data script ec2WebUserData.tftpl
+resource "aws_launch_template" "grad_lab_1_asg_launch_temp" {
+  name          = "${var.project_name}-${var.environment}-instance-launch-template"
+  image_id      = data.aws_ami.latest_linux_2_AMI.image_id
+  instance_type = "t2.micro"
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.EC2_instance_profile.name
+  }
+
+  user_data = base64encode(templatefile("./src/ec2WebUserData.tftpl"),
+    {
+      bucket_name = "${var.project_name}-${var.environment}-website-files"
+  })
 }
